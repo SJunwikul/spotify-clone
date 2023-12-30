04f1447b546c0cbdd79c34d45a4d68841440611a
Jonan <84633272+SJunwikul@users.noreply.github.com>
2023-12-29 19:40:00 -0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
